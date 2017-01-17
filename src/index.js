283537de07ec9d1abd0c9d9aaf30fe71a5ce1318
DGrishkin <dgrishkin@competentum.ru>
2017-01-17 17:16:54 +0300

    added applying settings

--- src/index.js
+++ src/index.js
@@ -19,19 +19,28 @@ const KEYCODE = {
 class CgInput extends EventEmitter {
 
   /**
-   *
-   * @returns {*}
+   * @property {string} label
+   * @property {string} placeholder
+   * @property {string} preset          - input component has some presets for quick setup
+   * @property {string} restrict        - uses regular expressions syntax to filter the input data
+   * @property {boolean} disabled
+   * @property {boolean|object} spinner - settings for spinner
+   * @property {string|number} value    - initial value
+   * @property {boolean} required       - input check
+   * @property {function} formatter     - this setting allows you to edit the value visible
+   * @property {function} unformatter   - oppositely formatter setting
    * @constructor
    */
   static get DEFAULT_SETTINGS() {
     if (!this._DEFAULT_SETTINGS) {
       this._DEFAULT_SETTINGS = {
-        label: false,
-        placeholder: false,
+        container: null,
+        label: '',
+        placeholder: '',
+        value: '',
+        required: false,
         disabled: false,
         spinner: false,
-        value: false,
-        required: false,
         preset: 'text',
         restrict: '.',
         formatter: function (v) {
@@ -48,6 +57,16 @@ class CgInput extends EventEmitter {
     return this._DEFAULT_SETTINGS;
   }
 
+  /**
+   * Types of listeners
+   * @returns {{
+   *   FOCUS: string,
+   *   CHANGE: string,
+   *   INPUT: string,
+   *   BLUR: string
+   * }}
+   * @constructor
+   */
   static get EVENTS() {
     if (!this._EVENTS) {
       this._EVENTS = {
@@ -60,18 +79,28 @@ class CgInput extends EventEmitter {
     return this._EVENTS;
   }
 
+  /**
+   *
+   * @param {object} settings
+   */
   constructor(settings) {
     super();
 
-    this.settings = merge.recursive(true, templates[settings.preset], settings);
-    this.settings = merge.recursive(true, this.SETTINGS, this.settings);
-
     this._render();
+    this._applySettings(settings);
     this._init();
   }
 
   get SETTINGS() {
     return this.constructor.DEFAULT_SETTINGS;
+  }
+
+  /**
+   *
+   * @param {*} value
+   */
+  set value(value) {
+    this._value = value;
   }
 
   /**
@@ -84,44 +113,40 @@ class CgInput extends EventEmitter {
 
   /**
    *
-   * @param {*} value
-   */
-  set value(value) {
-    this._value = value;
-  }
-
-  /**
-   *
    * @param label
    */
   set label(label){
-    this.settings.label = label;
+    this._settings.label = label;
     this._elementLabel.innerHTML = label;
   }
 
   get label(){
-    return this.settings.label;
+    return this._settings.label;
   }
 
   /**
-   *
-   * @param value
+   * Setter placeholder
+   * @param {string|boolean} value
    */
   set placeholder(value){
-    this.settings.placeholder = value;
+    this._settings.placeholder = value;
     this._element.placeholder = value;
   }
 
+  /**
+   * Getter placeholder
+   * @returns {string|boolean}
+   */
   get placeholder(){
-    return this.settings.placeholder;
+    return this._settings.placeholder;
   }
 
   /**
-   *
-   * @param disable
+   * Disable input
+   * @param {boolean} disable
    */
   set disabled(disable){
-    this.settings.disabled = disable;
+    this._settings.disabled = disable;
     this._element.disabled = disable;
 
     let changeClass;
@@ -130,8 +155,12 @@ class CgInput extends EventEmitter {
     changeClass(this.container, 'disabled');
   }
 
+  /**
+   * Getter disabled
+   * @returns {boolean}
+   */
   get disabled(){
-    return this.settings.disabled;
+    return this._settings.disabled;
   }
 
   /**
@@ -139,13 +168,66 @@ class CgInput extends EventEmitter {
    * @param value
    */
   set restrict(value){
-    if(typeof value === 'string'){
-      this.settings.restrict = value;
-    }
+    this._settings.restrict = this.constructor._fixSetting('restrict', value);
   }
 
   get restrict(){
-    return this.settings.restrict;
+    return this._settings.restrict;
+  }
+
+  /**
+   * Setter container
+   * @param {string|element} container
+   */
+  set container(container){
+    this._container = this.constructor._fixSetting('container', container);
+    this._container.appendChild(this._rootElement);
+  }
+
+  /**
+   * Getter container
+   * @returns {*}
+   */
+  get container(){
+    return this._container;
+  }
+
+  /**
+   *
+   * @param settings
+   */
+  set spinner(settings){
+    settings = this.constructor._fixSetting('spinner', settings);
+
+    if(settings){
+      if(!this._spinner){
+        this._spinner = new Spinner(this, settings);
+      }
+
+      //this._spinner.applySettings(settings);
+    } else {
+      // need to hide/remove spinner
+    }
+  }
+
+  get spinner(){
+    return this._spinner;
+  }
+
+  set formatter(formatter){
+    this._formatter = this.constructor._fixSetting('format', formatter);
+  }
+
+  get formatter(){
+    return this._formatter;
+  }
+
+  set unformatter(unformatter){
+    this._unformatter = this.constructor._fixSetting('format', unformatter);
+  }
+
+  get unformatter(){
+    return this._unformatter;
   }
 
   /**
@@ -199,12 +281,15 @@ class CgInput extends EventEmitter {
     this.emit(this.constructor.EVENTS.INPUT);
   }
 
-  _compare(){
-    return this.value == this._element.value;
-  }
-
+  /**
+   *
+   * @param value
+   * @param {string} pattern
+   * @returns {string}
+   * @private
+   */
   _restrict(value, pattern) {
-    pattern = pattern || this.settings.restrict;
+    pattern = pattern || this._settings.restrict;
 
     // create new regexp and find matches
     let regex = new RegExp(pattern, 'g');
@@ -220,7 +305,7 @@ class CgInput extends EventEmitter {
    * @private
    */
   _validate(value){
-    return this.settings.validate(value);
+    return this._settings.validate(value);
   }
 
   /**
@@ -233,7 +318,7 @@ class CgInput extends EventEmitter {
     value = value || this._element.value;
     value = value.toString();
 
-    return value ? this.settings.unformatter(value) : '';
+    return value ? this._settings.unformatter(value) : '';
   }
 
   /**
@@ -243,7 +328,7 @@ class CgInput extends EventEmitter {
    */
   _format() {
     if(this.value){
-      return this.settings.formatter(this.value);
+      return this._settings.formatter(this.value);
     }
 
     return this._element.value || '';
@@ -276,9 +361,9 @@ class CgInput extends EventEmitter {
    * Disable formatting input
    */
   disable() {
-    if (!this.settings.disabled) return;
+    if (!this._settings.disabled) return;
 
-    this.settings.disabled = true;
+    this._settings.disabled = true;
     this._element.disabled = true;
 
     utils.addClass(this.container, 'disabled');
@@ -288,9 +373,9 @@ class CgInput extends EventEmitter {
    * Enable formatting input
    */
   enable() {
-    if (this.settings.disabled) return;
+    if (this._settings.disabled) return;
 
-    this.settings.disabled = false;
+    this._settings.disabled = false;
     this._element.disabled = false;
 
     utils.removeClass(this.container, 'disabled');
@@ -300,38 +385,24 @@ class CgInput extends EventEmitter {
    * @private
    */
   _render() {
-    // save container to short link
-    this.container = this.settings.container;
-
-    this._element = utils.createHTML('<input/>');
-    this._elementLabel = utils.createHTML('<label></label>');
+    this._rootElement = document.createElement('div');
+    this._element = document.createElement('input');
+    this._elementLabel = document.createElement('label');
 
     // attach events
     this._element.addEventListener('focus', this._focus.bind(this));
     this._element.addEventListener('blur', this._blur.bind(this));
     this._element.addEventListener('input', this._input.bind(this));
 
-    // if user don't pass container in the settings
-    if (typeof this.container === 'undefined') {
-      this.container = utils.createHTML('<div></div>');
-    }
+    this._rootElement.appendChild(this._elementLabel);
+    this._rootElement.appendChild(this._element);
 
-    this.container.appendChild(this._elementLabel);
-    this.container.appendChild(this._element);
-
-    utils.addClass(this.container, INPUT_CLASS);
+    utils.addClass(this._rootElement, INPUT_CLASS);
   }
 
   _init() {
-    this._elementLabel.innerHTML = this.settings.label || '';
-    this._element.value = this.settings.value || '';
-
-    this.disable();
-
-    if (this.settings.spinner) {
-      this.spinner = new Spinner(this, this.settings.spinner);
-
-      this._element.addEventListener('keydown', (e) => {
+    this._element.addEventListener('keydown', (e) => {
+      if(this._spinner){
         switch (e.which || e.keyCode) {
           case KEYCODE.ARROW.UP:
             this.spinner.increase();
@@ -342,66 +413,99 @@ class CgInput extends EventEmitter {
             e.preventDefault();
             break;
         }
-      });
-    }
+      }
+    });
   }
 
-  /*
-   _input(){
-   let oldValue = this._element.oldValue || '';
-   let value = this._element.value;
-   let shift = 0;
-   let start;
-   let diff;
+  /**
+   *
+   * @param name
+   * @param setting
+   * @private
+   */
+  static _fixSetting(name, setting) {
+    switch(name){
+      // string part
+      case 'label':
+      case 'placeholder':
+      case 'preset':
+      case 'restrict':
+        if(typeof setting !== 'string'){
+          setting = '';
+        }
+        break;
+      case 'container':
+        if(!(setting instanceof HTMLElement)){
+          if(typeof setting === 'string'){
+            let element = document.querySelector(setting);
 
-   // slice numeric value
-   let regex = /[0-9\-\.]+/g;
-   let matches = value.match(regex);
-   let transformed = matches ? matches.join('') : '';
+            if(element){
+              setting = element;
+            }
+          } else {
+            setting = null;
+          }
+        }
+        break;
+      case 'disabled':
+        if(typeof setting !== 'boolean'){
+          return false;
+        }
+        break;
+      case 'required':
+        break;
+      case 'spinner':
+        if(typeof setting !== 'object'){
+          setting = false;
+        }
+        break;
+      case 'value':
+        break;
+      case 'formatter':
+      case 'unformatter':
+        if(typeof setting !== 'function'){
+          setting = this.formatter || this.SETTINGS.formatter;
+        }
+        break;
+    }
 
-   let minusMatches = (transformed.match(/[\-]/g) || []).length;
-   let dotMatches = (transformed.match(/[\.]/g) || []).length;
+    return setting;
+  }
 
-   if(dotMatches > 1){
-   var oldDotIndex = oldValue.indexOf('.');
-   var dotIndex = transformed.indexOf('.');
+  /**
+   *
+   * @param settings
+   * @private
+   */
+  static _fixSettings(settings) {
+    let name;
 
-   if(oldDotIndex === dotIndex){
-   transformed = transformed.substring(0, oldDotIndex) +
-   transformed.substring(oldDotIndex + 1);
-   } else {
-   shift++;
-   transformed = transformed.substring(0, dotIndex) + '.' +
-   transformed.substring(dotIndex + 1).replace(/[\.]/g, '');
-   }
-   }
+    for(name in settings){
+      if(settings.hasOwnProperty(name)){
+        settings[name] = this._fixSetting(name, settings[name]);
+      }
+    }
 
-   if(minusMatches){
-   var sign = (minusMatches > 1) ? '' : '-';
+    return settings;
+  }
 
-   // placed minus in the beginning of the line
-   transformed = sign + transformed.replace(/[-]/g, '');
-   }
+  _applySettings(settings){
+    let template = templates[settings.preset];
 
-   // calculate differense between input and transformed value
-   diff = value.length - transformed.length;
+    if(typeof template !== 'undefined'){
+      settings = merge.recursive(true, template, settings);
+    }
 
-   // write selection start position
-   start = this._element.selectionStart;
-   start = isNaN(start) ? value.length : start + shift;
-   start = (diff !== 0) ? start - diff : start;
+    this._settings = merge.recursive(true, this.SETTINGS, settings);
+    this._settings = this.constructor._fixSettings(settings);
 
-   this._element.value = transformed;
-   this._element.oldValue = transformed;
-
-   this._element.selectionStart = start;
-   this._element.selectionEnd = start;
-
-   if(this.value != transformed){
-   this.value = this._unformat(transformed);
-   }
-   }
-   * */
+    // call setters for settings which defined in DEFAULT_SETTINGS only
+    for (let key in this.SETTINGS) {
+      if (this.SETTINGS.hasOwnProperty(key)) {
+        this[key] = settings[key];
+      }
+    }
+  }
 }
 
 module.exports = CgInput;
